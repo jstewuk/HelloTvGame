4c5be13cd5d9186721283eb53c323c34fd4719f7
Jim Stewart <jim@stewartstuff.com>
2015-12-14 07:22:37 -0600

    Track touches

--- HelloTvGame/GameScene.swift
+++ HelloTvGame/GameScene.swift
@@ -19,23 +19,48 @@ class GameScene: SKScene {
         self.addChild(myLabel)
     }
     
+    private lazy var ship: SKSpriteNode = {
+        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        sprite.name = "Spaceship"
+        
+        sprite.xScale = 0.5
+        sprite.yScale = 0.5
+        return sprite
+    }()
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            guard let _ = ship.parent else {
+                ship.position = location
+                self.addChild(ship)
+                return
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            self.enumerateChildNodesWithName("Spaceship") { (node, _) -> Void in
+                if let sprite = node as? SKSpriteNode {
+                    let previousLocation = touch.previousLocationInNode(self)
+                    let delta = CGVectorMake(location.x - previousLocation.x, location.y - previousLocation.y)
+                    let deltaDist = sqrt(delta.dx*delta.dx + delta.dy*delta.dy)
+                    let ratio = deltaDist/(sqrt(self.size.height*self.size.height + self.size.width*self.size.width))
+                    let duration = Double(0.2 * ratio)
+                    let action = SKAction.moveBy(delta, duration: duration)
+                    sprite.runAction(action)
+                }
+            }
         }
     }
    
